efa3115afa2450c1ed13b45186584ee97766d860
Shubham Kumar <shubraj2323@gmail.com>
2026-08-11 23:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (1.88%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int low=0;
+        int high=n;
+        while(low<=high){
+            long long mid=(low+high)/2;
+            long long coins=mid*(mid+1)/2;
+            if(coins<=n){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};
